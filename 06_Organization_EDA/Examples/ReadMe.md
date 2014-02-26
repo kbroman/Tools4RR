0f3f5c0b96274af09379e1ccff220df73c694387
Karl Broman <kbroman@gmail.com>
2014-02-26 12:56:25 -0600

    Example for knitr spin(), with lecture 6

---
+++ 06_Organization_EDA/Examples/ReadMe.md
@@ -0,0 +1,10 @@
+### Example of an R file for knitr `spin()`
+
+`example.R` is a simple example of an
+R file with [Roxygen2](https://github.com/klutometis/roxygen)-like comments, 
+which `spin()` will convert to an R Markdown file, then to Markdown,
+then to html.
+
+`Makefile` is the [GNU make](http://www.gnu.org/software/make) file
+for using [knitr](http://yihui.name/knitr/) to convert the R
+file to html.
---
+++ 06_Organization_EDA/Examples/example.R
@@ -0,0 +1,25 @@
+#' This is a simple example of an R file for use with KnitR's spin().
+
+# This comment will appear as code rather than text.
+
+#' We'll start by setting the seed for the random number generator.
+set.seed(53079239)
+
+#' We'll first simulate some data with x ~ N(mu=10, sig=5) and
+#' y = 2x + e, where e ~ N(mu=0, sig=2)
+x <- rnorm(100, 10, 5)
+y <- 2*x + rnorm(100, 0, 2)
+
+#'Here's a scatterplot of the data.
+plot(x, y, pch=21, bg="slateblue", las=1)
+
+
+#' The estimated correlation between x and y is
+cor(x,y)
+
+#' Here is the result of regressing $y$ on $x$.
+out <- lm(y ~ x)
+summary(out)
+
+#' So note that the estimated slope for the regression line is
+out$coef[2]
