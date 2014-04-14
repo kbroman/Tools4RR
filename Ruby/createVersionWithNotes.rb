ee4f1c95726850e2fb431127e666c9bc87baf76c
Karl Broman <kbroman@gmail.com>
2014-04-14 14:59:04 -0500

    createVersionWithNotes: do substitutions just once (needed when I included code as examples)

--- Ruby/createVersionWithNotes.rb
+++ Ruby/createVersionWithNotes.rb
@@ -7,9 +7,18 @@ ofilename = ARGV.length > 1 ? ARGV[1] : ifilename.sub(".tex", "_withnotes.tex")
 ifile = File.open(ifilename)
 ofile = File.open(ofilename, "w")
 
+do_1st_sub = true
+do_2nd_sub = true
 ifile.readlines.each do |z|
-  z = z.sub("hide", "show") if /\\setbeameroption{hide notes}/ =~ z
-  z = z.sub("[12pt,t]", "[12pt,t,handout]") if /\\documentclass\[12pt,t\]{beamer}/ =~ z
+  if do_1st_sub and /\\setbeameroption{hide notes}/ =~ z
+    z = z.sub("hide", "show")
+    do_1st_sub = false
+  end
+
+  if do_2nd_sub and /\\documentclass\[12pt,t\]{beamer}/ =~ z
+    z = z.sub("[12pt,t]", "[12pt,t,handout]")
+    do_2nd_sub = false
+  end
 
   ofile.write("\\def\\notescolors{1}\n") if /^\\input{/ =~ z # add line saying to use notes colors
 
