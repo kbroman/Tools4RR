b7490f05168464786c1620b37303673160242832
Karl Broman <kbroman@gmail.com>
2014-04-22 00:23:38 -0500

    Cosmetic changes to test_convert?.py

--- 13_Python/Code/test_convert2.py
+++ 13_Python/Code/test_convert2.py
@@ -8,10 +8,10 @@ from convert2 import *
 
 class check_parse_genotype(unittest.TestCase):
   def test_parse_genotype(self):
-    self.assertEqual(parse_genotype("       "), "0 0")
-    self.assertEqual(parse_genotype("100/98 "), "100 98")
-    self.assertEqual(parse_genotype("90/96  "), "90 96")
-    self.assertEqual(parse_genotype("90/ 96  "), "90 96")
+    self.assertEqual(parse_genotype("       "),   "0 0")
+    self.assertEqual(parse_genotype("100/98 "),   "100 98")
+    self.assertEqual(parse_genotype("90/96  "),   "90 96")
+    self.assertEqual(parse_genotype("90/ 96  "),  "90 96")
     self.assertEqual(parse_genotype("  3 / 8  "), "3 8")
 
 if __name__ == '__main__':
