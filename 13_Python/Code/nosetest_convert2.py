d8faa9ba4ae78700a5f1268f5d581e0fff2665b6
Karl Broman <kbroman@gmail.com>
2014-04-22 00:23:53 -0500

    Add another test to nosetest_convert2.py
     - Also some cosmetic changes

--- 13_Python/Code/nosetest_convert2.py
+++ 13_Python/Code/nosetest_convert2.py
@@ -6,7 +6,8 @@ from nose.tools import assert_equal
 from convert2 import *
 
 def test_parse_genotype():
-  assert_equal(parse_genotype("       "), "0 0")
-  assert_equal(parse_genotype("100/98 "), "100 98")
-  assert_equal( parse_genotype("90/96  "), "90 96")
-  assert_equal(parse_genotype("90/ 96  "), "90 96")
+  assert_equal(parse_genotype("       "),   "0 0")
+  assert_equal(parse_genotype("100/98 "),   "100 98")
+  assert_equal(parse_genotype("90/96  "),   "90 96")
+  assert_equal(parse_genotype("90/ 96  "),  "90 96")
+  assert_equal(parse_genotype("  3 / 8  "), "3 8")
